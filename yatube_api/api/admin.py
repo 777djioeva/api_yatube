b9214a33f85ec36e0ca5cf71345a0dc6b890e381
777djioeva <777djioeva@gmail.com>
2026-05-13 13:01:19 +0300

    fix: Реализован API для Yatube

--- yatube_api/api/admin.py
+++ yatube_api/api/admin.py
@@ -1,3 +1,1 @@
-from django.contrib import admin
-
-# Register your models here.
+# Файл оставлен пустым, так как админка для API не требуется
--- yatube_api/api/models.py
+++ yatube_api/api/models.py
@@ -1,3 +1,1 @@
-from django.db import models
-
-# Create your models here.
+# Все модели уже определены в приложении posts
